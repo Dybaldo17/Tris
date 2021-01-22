f4bb07a5a16863d37239c674f8ed2a19ba9753dd
Federico <carrabba.federico@gmail.com>
2021-01-22 16:46:14 +0100

    reset

---
+++ app/src/main/java/com/carrabba/tris/GiocoActivity.java
@@ -0,0 +1,14 @@
+package com.carrabba.tris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiocoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gioco);
+    }
+}
