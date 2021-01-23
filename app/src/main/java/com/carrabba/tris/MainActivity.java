0d954d636611c9d2be8dc8619c3b1d90785d3708
Federico <carrabba.federico@gmail.com>
2021-01-23 17:54:40 +0100

    login button

--- app/src/main/java/com/carrabba/tris/MainActivity.java
+++ app/src/main/java/com/carrabba/tris/MainActivity.java
@@ -2,13 +2,31 @@ package com.carrabba.tris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apriGioco();
+            }
+        });
+    }
+
+    public void apriGioco() {
+        Intent intent = new Intent(this, GiocoActivity.class);
+        startActivity(intent);
     }
 }
