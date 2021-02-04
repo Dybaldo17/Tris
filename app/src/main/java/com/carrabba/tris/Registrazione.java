cd72a20e1e0f0c40691db646d59eb068807e2d39
Federico <carrabba.federico@gmail.com>
2021-02-04 09:41:40 +0100

    login

--- app/src/main/java/com/carrabba/tris/Registrazione.java
+++ app/src/main/java/com/carrabba/tris/Registrazione.java
@@ -79,7 +79,7 @@ public class Registrazione extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(Registrazione.this, "Utente creato", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), GiocoActivity.class));
+                            startActivity(new Intent(getApplicationContext(), Login.class));
                         } else {
                             Toast.makeText(Registrazione.this, "Errore!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
